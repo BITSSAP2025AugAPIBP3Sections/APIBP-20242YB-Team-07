c7098f9b12afcaba2a77b5b514825b1f331f7738
Sachit Tarway <86160848+Sachittarway@users.noreply.github.com>
2025-10-21 18:34:38 +0530

    Initial maven setup for services (#3)

---
+++ backend/nutrition-service/src/test/java/com/cooknect/nutrition_service/NutritionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooknect.nutrition_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
