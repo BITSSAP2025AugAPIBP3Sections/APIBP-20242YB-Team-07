4b6665b02bccf9f3ea5aafbe6e9674c71bdc8af0
Sachittarway <work.sachittarway@gmail.com>
2025-11-02 19:35:03 +0530

    Spring security issue fixed

--- backend/gateway-service/src/main/java/com/cooknect/gateway_service/configuration/SecurityConfig.java
+++ backend/gateway-service/src/main/java/com/cooknect/gateway_service/configuration/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,6 +17,15 @@ public class SecurityConfig {
 
     @Autowired
     private JWTFilter jwtAuthFilter;
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        return authentication -> {
+            // Since the Gateway does not perform user authentication (it validates tokens),
+            // this placeholder is to throw a generic exception if it's ever invoked.
+            throw new AuthenticationServiceException("Authentication is handled by JWT filter.");
+        };
+    }
 
     @Bean
     @Order(1)
