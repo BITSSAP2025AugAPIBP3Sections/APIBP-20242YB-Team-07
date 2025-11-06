2bcc9991b58d57b8a442e193890be2a0ff5c6ea9
kanakphulwani <76205846+kanakphulwani@users.noreply.github.com>
2025-11-06 18:36:18 +0530

    feat(user-service): User Service API Documentation (#72)

--- backend/user-service/src/main/java/com/cooknect/user_service/controller/UserController.java
+++ backend/user-service/src/main/java/com/cooknect/user_service/controller/UserController.java
@@ -3,6 +3,9 @@ package com.cooknect.user_service.controller;
 import com.cooknect.user_service.dto.UsersDTO;
 import com.cooknect.user_service.model.UserModel;
 import com.cooknect.user_service.service.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -32,36 +35,42 @@ public class UserController {
     }
 
     @GetMapping("/")
+    @Operation(summary = "Get all users", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<List<UsersDTO>> getAllUsers() {
         List<UsersDTO> users = service.getAllUsers();
         return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get user by ID", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<UsersDTO> getUserById(@PathVariable Long id) {
         UsersDTO users = service.getUserById(id);
         return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{id}/health-goal")
+    @Operation(summary = "Get user health goal", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<Map<String, String>> getHealthGoal(@PathVariable Long id){
         Map<String, String> health = service.getHealthGoal(id);
         return ResponseEntity.ok(health);
     }
 
     @GetMapping("/{id}/dietary-preference")
+    @Operation(summary = "Get user dietary preference", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<Map<String, String>> getDietaryPreference(@PathVariable Long id){
         Map<String, String> diet = service.getDietaryPreference(id);
         return ResponseEntity.ok(diet);
     }
 
     @GetMapping("/{id}/cuisine-preference")
+    @Operation(summary = "Get user cuisine preference", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<Map<String, Object>> getCuisinePreference(@PathVariable Long id){
         Map<String, Object> cuisine = service.getUserCuisinePreferences(id);
         return ResponseEntity.ok(cuisine);
     }
 
     @PostMapping("/login")
+    @Operation(summary = "User login", description = "Authenticate a user", security = {})
     public Map<String, String> loginUser(@RequestBody UserModel user){
         String token = service.verify(user);
         Map<String, String> response = new HashMap<>();
@@ -70,6 +79,7 @@ public class UserController {
     }
 
     @PostMapping("/register")
+    @Operation(summary = "User registration", description = "Register a new user", security = {})
     public ResponseEntity<?> createUser(@RequestBody UserModel user) {
         if(user.getPassword() == null || user.getPassword().isEmpty()){
             return ResponseEntity.badRequest().body(Map.of("error", "Password cannot be empty"));
@@ -89,6 +99,7 @@ public class UserController {
 
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update user by ID", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody UsersDTO userDTO,HttpServletRequest request) {
         String userEmailHeader = request.getHeader("X-User-Email");
         if (userDTO.getEmail() == null || userDTO.getEmail().isEmpty()){
@@ -105,6 +116,7 @@ public class UserController {
     }
 
     @PutMapping("/{id}/preferences")
+    @Operation(summary = "Update user preferences", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<UsersDTO> updatePreferences(@PathVariable Long id, UsersDTO userDTO,HttpServletRequest request){
         String userEmailHeader = request.getHeader("X-User-Email");
         UsersDTO updatedUser = service.updatePreferences(id, userDTO, userEmailHeader);
@@ -112,6 +124,7 @@ public class UserController {
     }
 
     @PutMapping("/{id}/health-preference")
+    @Operation(summary = "Update health preference", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<UsersDTO> updateHealthGoalPreference(@PathVariable Long id, UsersDTO userDTO,HttpServletRequest request){
         String userEmailHeader = request.getHeader("X-User-Email");
         UsersDTO updatedUser = service.updateHealthGoalPreference(id, userDTO, userEmailHeader);
@@ -119,6 +132,7 @@ public class UserController {
     }
 
     @PutMapping("/{id}/dietary-preference")
+    @Operation(summary = "Update dietary preference", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<UsersDTO> updateDietaryPreference(@PathVariable Long id, UsersDTO userDTO,HttpServletRequest request){
         String userEmailHeader = request.getHeader("X-User-Email");
         UsersDTO updatedUser = service.updateDietaryPreference(id, userDTO, userEmailHeader);
@@ -126,6 +140,7 @@ public class UserController {
     }
 
     @PutMapping("/{id}/cuisine-preference")
+    @Operation(summary = "Update cuisine preference", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<UsersDTO> updateCuisinePreference(@PathVariable Long id, UsersDTO userDTO,HttpServletRequest request){
         String userEmailHeader = request.getHeader("X-User-Email");
         UsersDTO updatedUser = service.updateCuisinePreference(id, userDTO, userEmailHeader);
@@ -133,6 +148,7 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete user by ID", security = @SecurityRequirement(name = "bearerAuth"))
     public ResponseEntity<Void> deleteUser(@PathVariable Long id,HttpServletRequest request) {
         String userEmailHeader = request.getHeader("X-User-Email");
         service.deleteUser(id,userEmailHeader);
