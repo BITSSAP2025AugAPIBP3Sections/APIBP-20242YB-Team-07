917625372f6ce2f37ade78f2566227dc61d7af8e
sharma-ishaan <105979615+sharma-ishaan@users.noreply.github.com>
2025-11-13 05:57:17 +0530

    feat(recipe-service): gRPC service for getRecipeByID (#100)

--- backend/recipe-service/src/main/java/com/cooknect/recipe_service/repository/RecipeRepository.java
+++ backend/recipe-service/src/main/java/com/cooknect/recipe_service/repository/RecipeRepository.java
@@ -3,6 +3,7 @@ package com.cooknect.recipe_service.repository;
 import com.cooknect.recipe_service.model.Recipe;
 import com.cooknect.recipe_service.model.Cuisine;
 import com.cooknect.recipe_service.model.Ingredient;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,5 +18,7 @@ public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     List<Recipe> findByTitleContainingIgnoreCase(String title);
     List<Recipe> findByUsername(String username);
     Optional<Recipe> findByUsernameAndId(String username, Long id);
+    @EntityGraph(attributePaths = {"ingredients", "comments"})
+    Optional<Recipe> findById(Long id);
 
 }
