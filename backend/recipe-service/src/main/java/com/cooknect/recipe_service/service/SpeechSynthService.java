615639e4bc0d1571c73523f173418daa1b5e1738
I578048 <nivedita.tibrewala@sap.com>
2025-11-14 22:22:46 +0530

    feat(recipe-service): Text To Speech to the Recipe Service

--- backend/recipe-service/src/main/java/com/cooknect/recipe_service/service/SpeechSynthService.java
+++ backend/recipe-service/src/main/java/com/cooknect/recipe_service/service/SpeechSynthService.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import java.nio.file.Path;
 
 import org.springframework.beans.factory.annotation.Value;
