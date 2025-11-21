f7ab7441046b0d29e743ed639a151ff6ee0bddfa
ritika1408 <ritikamalhotra611@gmail.com>
2025-11-21 10:16:25 +0530

    feat(user-service): Add PaginationUtils for handling pagination logic with PageRequestDTO and PageResponseDTO

---
+++ backend/user-service/src/main/java/com/cooknect/user_service/utils/PaginationUtils.java
@@ -0,0 +1,32 @@
+package com.cooknect.user_service.utils;
+import com.cooknect.common.dto.PageRequestDTO;
+import com.cooknect.common.dto.PageResponseDTO;
+import org.springframework.data.domain.*;
+
+public class PaginationUtils {
+    public static Pageable toPageable(PageRequestDTO req) {
+
+        Sort sort = req.getDirection().equalsIgnoreCase("asc")
+                ? Sort.by(req.getSortBy()).ascending()
+                : Sort.by(req.getSortBy()).descending();
+
+        return PageRequest.of(req.getPage(), req.getSize(), sort);
+    }
+
+    public static <T> PageResponseDTO<T> toPageResponse(Page<T> page) {
+
+        PageResponseDTO<T> dto = new PageResponseDTO<>();
+
+        dto.setContent(page.getContent());
+        dto.setPage(page.getNumber());
+        dto.setSize(page.getSize());
+        dto.setTotalElements(page.getTotalElements());
+        dto.setTotalPages(page.getTotalPages());
+        dto.setSort(page.getSort().toString());
+
+        return dto;
+    }
+}
+
+
+
